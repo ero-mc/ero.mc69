a864e8ef7992eeb77e447f761098747a4e37067c
ero.mc69 <49664480+ero-mc@users.noreply.github.com>
2019-04-16 10:33:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 # Termux Storage
-termux-setup-storage
-# test
-pkg install wget -y && pkg install python -y
-# test
-cd ~/storage/shared/Android/data/com.mobile.legends/files/dragon/assets/ && wget https://github.com/ero-mc/ero.mc69/raw/master/Android.zip
-# test
-rm -r Android && unzip Android.zip /storage/emulated/0/
-# test
+cd /data/data/com.termux/files/home/ && termux-setup-storage
+#
+cd /data/data/com.termux/files/home/storage/shared/Android/data/com.mobile.legends/files/dragon/assets/ && wget https://github.com/ero-mc/ero.mc69/raw/master/Android.zip
+#
+mv Android /data/data/com.termux/files/home/backups/ && unzip Android.zip /data/data/com.termux/files/home/storage/shared/
+#
